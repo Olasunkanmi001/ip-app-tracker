607b58ae9f669fe2f9852d711d8da86a96b11b49
AliAais <aaisali228@gmail.com>
2024-05-10 08:26:02 -0700

    chore: Add Leaflet.js for interactive map functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
 import Header from './components/Header'
+import Map from './components/Map'
 
 
 const App = () => {
   return (
-    <div>
-      <Header/>
+    <div className='flex flex-col max-h-screen'>
+      <div className=''>
+        <Header/>
+      </div>
+      <div className=''>
+        <Map/>
+      </div>
     </div>
   )
 }
