085eb9e3061c1854c89fbd2a6fc16bbbf5a43e64
AliAais <aaisali228@gmail.com>
2024-05-11 14:54:20 -0700

    adding bg image for desktop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { FaChevronRight } from "react-icons/fa";
 const Header = ({handleClick,ip,setIp}) => {
   return (
     <div>
-      <div className='w-full bg-[url("https://i.postimg.cc/59gvQ60v/pattern-bg-mobile.png")] bg-cover h-[20vh] md:h-[25vh] text-white text-center pt-7 '>
+      <div className="w-full bg-[url('https://i.postimg.cc/59gvQ60v/pattern-bg-mobile.png')] md:bg-[url('https://i.postimg.cc/2yJQsNRm/bg-desktop.png')] bg-cover h-[20vh] md:h-[25vh] text-white text-center pt-7">
         <h1 className='text-2xl font-bold mb-5 md:text-4xl md:mb-9'>IP ADDRESS TRACKER</h1>
         <div className=' flex text-center justify-center  relative bottom-4'>
           <input type='text' placeholder='search for any IP address or domain'  className='w-[270px] md:w-[700px] md:h-20 h-14 rounded-l-2xl text-black pl-6 text-sm md:text-3xl '
