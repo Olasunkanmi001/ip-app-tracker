9e89f49969a8cab0faa3969e0c277f27cc0430db
AliAais <aaisali228@gmail.com>
2024-05-11 04:28:02 -0700

    chore: Update IP address search functionality and integrate with API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import Header from './components/Header'
 import Map from './components/Map'
+import { useState , useCallback } from 'react'
 
 
 const App = () => {
+
+  const [ip,setIp]=useState('');
+  const [data,setData]=useState([]);
+
+  const handleClick=async()=>{
+    const res=await fetch(`https://geo.ipify.org/api/v1?apiKey=${import.meta.env.VITE_API_KEY}&${ip}`);
+    const data=await res.json();
+    setData(data);
+  };
+
   return (
     <div className='flex flex-col max-h-screen'>
       <div className=''>
-        <Header/>
+        <Header handleClick={handleClick} ip={ip} setIp={setIp} />
       </div>
       <div className=''>
-        <Map/>
+        <Map data={data}/>
       </div>
     </div>
   )
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,21 +1,35 @@
-import { MapContainer } from "react-leaflet/MapContainer";
-import { TileLayer } from "react-leaflet/TileLayer";
-import { Popup, Marker } from "react-leaflet";
+/* eslint-disable react/display-name */
+/* eslint-disable react/prop-types */
+import React, { memo, useEffect, useRef } from 'react';
+import { MapContainer, TileLayer, Popup, Marker, useMap } from 'react-leaflet';
 
-const Map = () => {
+// Function to create a component that updates the map's center
+const SetView = ({ center, zoom }) => {
+  const map = useMap(); // useMap hook to access the map instance
+  map.setView(center, zoom);
+  return null;
+};
+
+const Map = memo(({ data }) => {
+  const position = [data?.location?.lat || 51.505, data?.location?.lng || -0.09];
+  const mapRef = useRef(null);
+
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer center={position} zoom={13} scrollWheelZoom={false} ref={mapRef} whenCreated={mapInstance => mapRef.current = mapInstance}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      {data?.location && (
+        <>
+          <Marker position={position}>
+            <Popup>Here he is</Popup>
+          </Marker>
+          <SetView center={position} zoom={13} />
+        </>
+      )}
     </MapContainer>
   );
-};
+});
 
 export default Map;
